1f6199f8b489531be28d971a50bb42da3f7fe00e
wilfredlopez <cubamc@gmail.com>
2021-04-29 17:44:08 -0400

    targeting es5

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ interface CorsRes {
 
 type CorsNextFunc = (err?: any) => any
 
-export default function cors<T extends CorsRequest = CorsRequest, Res extends CorsRes = CorsRes, Next extends CorsNextFunc = CorsNextFunc>(options: CorsOptions | CorsOptionsDelegate<T> | undefined = {}): (
+function cors<T extends CorsRequest = CorsRequest, Res extends CorsRes = CorsRes, Next extends CorsNextFunc = CorsNextFunc>(options: CorsOptions | CorsOptionsDelegate<T> | undefined = {}): (
     req: T,
     res: Res,
     next: Next
@@ -298,3 +298,5 @@ export default function cors<T extends CorsRequest = CorsRequest, Res extends Co
 }
 
 
+
+export default cors
